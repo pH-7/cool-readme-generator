4beeaf550ca568022ef52fe1cbd4ee2bc69e9c69
Pierre-Henry Soria <hi@ph7.me>
2021-09-25 22:20:45 +0930

    Add the basic necessary files to generate a README markedown file

---
+++ bin/cmd.js
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+
+const README_FILENAME = 'README.md';
+
+const generateMarkdown = require('./markdownGenerator');
+
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: 'README Heading',
+        name: 'heading',
+    },
+    {
+        type: 'input',
+        message: 'Project description',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Installation requirements?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'How to use it?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Your Full Name?',
+        name: 'author',
+        default: 'Pierre-Henry Soria'
+    },
+    {
+        type: 'input',
+        message: 'Your Email?',
+        name: 'email',
+        default: 'hi@ph7.me',
+    },
+    {
+        type: 'input',
+        message: 'Your Twitter username?',
+        name: 'twitter',
+        default: 'phenrysay'
+    },
+    {
+        type: 'input',
+        message: 'Your Personal Webpage?',
+        name: 'website',
+        default: 'https://ph7.me'
+    },
+    {
+        type: 'input',
+        message: 'GitHu account?',
+        name: 'github',
+        default: 'pH-7',
+    },
+    {
+        type: 'list',
+        message: 'License?',
+        name: 'license',
+        choices: ['MIT', 'ISC', 'MPL', 'GPL', 'AGPL'],
+        default: 'MIT'
+    }
+
+  ])
+  .then((data) => {
+      const fullReadmePath = path.resolve(__dirname, `../${README_FILENAME}`);
+      
+      fs.writeFile(fullReadmePath, generateMarkdown(data), (err) => err 
+        ? console.log(err) 
+        : console.log(`Your ${README_FILENAME} has been generated at: ${fullReadmePath}! 🚀`)
+    );
+  })
---
+++ bin/markdownGenerator.js
@@ -0,0 +1,39 @@
+function generateMarkdown(data) {
+    return ` 
+# ${data.heading}
+
+
+## 📄 Description
+${data.description}
+
+
+## 🛠 Installation
+${data.installation}
+
+
+## 🎮 Usage
+${data.usage}
+
+
+## 😋 Who cooked it?
+
+[![Pierre-Henry Soria](https://avatars0.githubusercontent.com/u/1325411?s=200)](${data.website} "My personal website :-)")
+
+
+[![@${data.twitter}][twitter-image]][https://twitter.com/${data.twitter}] [![${data.github}][github-image]][https://github.com/${data.github}]
+
+**[Pierre-Henry Soria][${data.website}]**, a highly pragmatic passionate, zen &amp; cool software engineer 😊
+
+
+## ⚖️ License
+
+Generously distributed under the *${data.license}*
+
+
+<!-- GitHub's Markdown reference links -->
+[twitter-image]: https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white
+[github-image]: https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white
+`
+}
+
+module.exports = generateMarkdown;
