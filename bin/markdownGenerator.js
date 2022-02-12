22bdf968a4a0705060cdac964dca30f5c16eef19
♚ PH⑦ de Soria™♛ <hi@ph7.me>
2022-02-12 13:18:10 +1030

    Add Powered By comment at bottom of generated file

--- bin/markdownGenerator.js
+++ bin/markdownGenerator.js
@@ -76,6 +76,8 @@ ${video}
 <!-- GitHub's Markdown reference links -->
 [twitter-image]: https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white
 [github-image]: https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white
+
+<!-- README generated with: https://github.com/pH-7/cool-readme-generator -->
 `;
 }
 
